cfaeb40d6e56d5cbe8d721d46a3314821bb7cb4b
exform <mmillwood256@gmail.com>
2023-07-25 16:55:52 -0500

    boop

--- keyboards/fingerpunch/barobord/keymaps/default/keymap.c
+++ keyboards/fingerpunch/barobord/keymaps/default/keymap.c
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
 [_QWERTY] = LAYOUT_barobord(
   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,
-  LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN),
+  KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,    KC_J,         KC_K,         KC_L,         KC_SCLN,
   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
-  _______,      _______,      LOWER,        KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       RAISE,      _______,      _______,
+  _______,      _______,      KC_GRV,       KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       KC_EQL,      _______,      _______,
                                                           KC_MUTE,      LCTL(KC_BSPC)  // These are for the rotary encoders
 ),
 
